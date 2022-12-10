36c9b65843016c510a0bb69b1b74e9677d79f8d7
Dimitar Yanev <dimitar.m.yanev@gmail.com>
2022-12-10 17:29:16 +0200

    removed ts-ignore

--- src/components/hoc/Layout/components/Header/Header.tsx
+++ src/components/hoc/Layout/components/Header/Header.tsx
@@ -3,6 +3,5 @@ import { wrap } from '../../../../../utils/globalUtils'
 import useHeader from './hooks/useHeader'
 import { HeaderView } from './views/HeaderView'
 
-// @ts-ignore
-const Header: FC = wrap(HeaderView, useHeader)
+const Header: FC<undefined> = wrap(HeaderView, useHeader)
 export default Header
