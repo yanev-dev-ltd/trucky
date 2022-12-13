d2926f5b97b6b54ff3181dc6ddb39af4e35f2ce0
Dimitar Yanev <dimitar.m.yanev@gmail.com>
2022-12-13 20:20:30 +0200

    minor css fixes

--- src/components/vehicles/Vehicles.tsx
+++ src/components/vehicles/Vehicles.tsx
@@ -1,57 +1,68 @@
-import { useMemo } from 'react'
-import Link from 'next/link'
-import Table from '../common/Table/Table'
-import { Add, FormatListBulleted } from '@mui/icons-material';
-import { FormattedMessage } from 'react-intl';
-import { TextField, CircularProgress, Button, Box, Typography } from '@mui/material'
+import { useMemo } from "react";
+import Link from "next/link";
+import Table from "../common/Table/Table";
+import { Add, FormatListBulleted } from "@mui/icons-material";
+import { FormattedMessage } from "react-intl";
+import {
+  TextField,
+  CircularProgress,
+  Button,
+  Box,
+  Typography,
+} from "@mui/material";
 
-import types from '../../api/types';
-import drivers from '../../api/drivers';
+import types from "../../api/types";
+import drivers from "../../api/drivers";
 
-import sx from './styles/Vehicles.sx'
-import useVehicles from './hooks/useVehicles'
-import { Vehicle } from './types'
+import sx from "./styles/Vehicles.sx";
+import useVehicles from "./hooks/useVehicles";
+import { Vehicle } from "./types";
 
 export const Vehicles = () => {
-    const vehicles = useVehicles()
-  const columns = useMemo(() => [
-    {
-      Header: <FormattedMessage id='app.Name' />,
-      id: 'name',
-      accessor: (v: Vehicle) => v.name
-    },
-    {
-      Header: <FormattedMessage id='app.Type' />,
-      id: 'type',
-      accessor: (v: Vehicle) => types.find(t => t.id === v.type)?.name
-    },
-    {
-      Header: <FormattedMessage id='app.Driver' />,
-      id: 'driver',
-      accessor: (v: Vehicle) => drivers.find(d => d.id === v.driver)?.name
-    },
-    {
-      Header: <FormattedMessage id='app.Route' />,
-      id: 'route',
-      accessor: (v: Vehicle) => v.route || '-'
-    },
-    {
-      Header: <FormattedMessage id='app.Details' />,
-      id: 'details',
-      accessor: v =>
-        <Button
-          component={Link}
-          href={`/vehicles/${v.key}`}
-          variant='outlined'
-          startIcon={<FormatListBulleted />}
-        ><FormattedMessage id='app.Details' />
-        </Button>
-    }
-  ], [])
+  const vehicles = useVehicles();
+  const columns = useMemo(
+    () => [
+      {
+        Header: <FormattedMessage id="app.Name" />,
+        id: "name",
+        accessor: (v: Vehicle) => v.name,
+      },
+      {
+        Header: <FormattedMessage id="app.Type" />,
+        id: "type",
+        accessor: (v: Vehicle) => types.find((t) => t.id === v.type)?.name,
+      },
+      {
+        Header: <FormattedMessage id="app.Driver" />,
+        id: "driver",
+        accessor: (v: Vehicle) => drivers.find((d) => d.id === v.driver)?.name,
+      },
+      {
+        Header: <FormattedMessage id="app.Route" />,
+        id: "route",
+        accessor: (v: Vehicle) => v.route || "-",
+      },
+      {
+        Header: <FormattedMessage id="app.Details" />,
+        id: "details",
+        accessor: (v) => (
+          <Button
+            component={Link}
+            href={`/vehicles/${v.key}`}
+            variant="outlined"
+            startIcon={<FormatListBulleted />}
+          >
+            <FormattedMessage id="app.Details" />
+          </Button>
+        ),
+      },
+    ],
+    []
+  );
 
   // TODO : end day of payment
 
-  if (vehicles?.[0].key === 'loading') {
+  if (vehicles?.[0].key === "loading") {
     return (
       <Box sx={sx.loading}>
         <CircularProgress />
@@ -61,22 +72,37 @@ export const Vehicles = () => {
 
   return (
     <Box>
-      <Box display='flex' justifyContent='space-between' sx={sx.padding}>
-        <Typography variant='h6'><FormattedMessage id='app.Vehicles' /></Typography>
+      <Box display="flex" justifyContent="space-between" sx={sx.padding}>
+        <Typography variant="h6">
+          <FormattedMessage id="app.Vehicles" />
+        </Typography>
         <TextField
-          variant='outlined'
-          label={<FormattedMessage id='app.SearchVehicles' />}
-          size='small'
+          variant="outlined"
+          label={<FormattedMessage id="app.SearchVehicles" />}
+          size="small"
         />
-        <Button variant='contained' color='primary' startIcon={<Add />}><FormattedMessage id='app.addVehicle' /></Button>
+        <Button variant="contained" color="primary" startIcon={<Add />}>
+          <FormattedMessage id="app.addVehicle" />
+        </Button>
       </Box>
-      {Array.isArray(vehicles) && vehicles.length > 0 && <Table columns={columns} data={vehicles} name='vehicles' />}
+      {Array.isArray(vehicles) && vehicles.length > 0 && (
+        <Table columns={columns} data={vehicles} name="vehicles" />
+      )}
       {Array.isArray(vehicles) && vehicles.length === 0 && (
-        <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column' style={{ height: 'calc(100vh - 54px - 68px)' }}>
-          <Typography variant='h5' sx={sx.padding}><FormattedMessage id='app.noVehicles' /></Typography>
-        </Box>)}
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          style={{ height: "calc(100vh - 54px - 72px)" }}
+        >
+          <Typography variant="h5" sx={sx.padding}>
+            <FormattedMessage id="app.noVehicles" />
+          </Typography>
+        </Box>
+      )}
     </Box>
-  )
-}
+  );
+};
 
 // export VehiclesView;
