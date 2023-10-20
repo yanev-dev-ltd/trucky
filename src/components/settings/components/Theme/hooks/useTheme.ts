2aae8fbda9c9b9217da5986441950f8a38d39ecd
Dimitar Yanev <dimitar.m.yanev@gmail.com>
2023-10-20 21:06:09 +0300

    added errors for settings

--- src/components/settings/components/Theme/hooks/useTheme.ts
+++ src/components/settings/components/Theme/hooks/useTheme.ts
@@ -1,11 +1,17 @@
 import { useCallback, ChangeEvent } from 'react'
 import { firestore, auth } from '@/services/firebase'
 import { updateDoc, doc } from 'firebase/firestore'
+import { useSnackbar } from 'notistack'
 
 const useTheme = () => {
+    const { enqueueSnackbar } = useSnackbar()
     const handleChange = useCallback(async (event: ChangeEvent<HTMLInputElement>) => {
         if (!auth?.currentUser?.uid) return
-        await updateDoc(doc(firestore, 'settings', auth.currentUser.uid), { theme: (event.target as HTMLInputElement).value })
+        try {
+            await updateDoc(doc(firestore, 'settings', auth.currentUser.uid), { theme: (event.target as HTMLInputElement).value })
+        } catch (error) {
+            enqueueSnackbar((error as Error).message, { variant: 'error', persist: true })
+        }
     }, [])
 
     return { handleChange }
