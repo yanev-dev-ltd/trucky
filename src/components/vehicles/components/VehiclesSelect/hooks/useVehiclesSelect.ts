cc650c4feffdaec71825d2602f37478fd920733e
Dimitar Yanev <dimitar.m.yanev@gmail.com>
2023-12-20 19:38:11 +0200

    setVehiclesRedux on vehiclesselect

--- src/components/vehicles/components/VehiclesSelect/hooks/useVehiclesSelect.ts
+++ src/components/vehicles/components/VehiclesSelect/hooks/useVehiclesSelect.ts
@@ -1,11 +1,32 @@
 import { Vehicle } from '@/components/vehicles/types'
 import { useVehiclesSelectProps } from '../types'
 import { RootState } from '@/store/store'
-import { useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import { useEffect, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { collection, query, where, onSnapshot } from 'firebase/firestore'
+import { firestore, auth } from '@/services/firebase'
+import { setVehicles as setVehiclesRedux } from '@/components/vehicles/redux'
 
 const useVehiclesSelect = ({ vehicles, setVehicles, sx, multiple }: useVehiclesSelectProps) => {
     const allVehicles = useSelector((state: RootState) => state.vehicles)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!auth.currentUser?.uid) {
+            return
+        }
+        const q = query(collection(firestore, 'vehicles'), where('userId', '==', auth.currentUser?.uid))
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const vehicles: Vehicle[] = []
+            querySnapshot.forEach((doc) => {
+                vehicles.push({...doc.data() as Vehicle, key: doc.id})
+            })
+            dispatch(setVehiclesRedux(vehicles))
+        })
+        return () => {
+            unsubscribe()
+        } 
+    }, [auth.currentUser?.uid])
 
     const selectedValue = useMemo(() => {
         if (vehicles && multiple) {
