efe80c7dda244b8649e433c252a98cb10f135722
Dimitar Yanev <dimitar.m.yanev@gmail.com>
2023-12-26 20:53:56 +0200

    locales pipeline fix

--- src/pages/registration/[[...locale]].tsx
+++ src/pages/registration/[[...locale]].tsx
@@ -1,11 +1,14 @@
 import { Registration as RegistrationComponent } from '@/components/registration/Registration'
 import { useRouter } from 'next/router'
+import { Locales } from '../../types/settings'
 
 export default function Registration(): JSX.Element {
     const router = useRouter()
     const locale =
         router.isReady && router.query.locale && router.query.locale.length > 0
-            ? router.query.locale[0]
-            : 'en'
+            ? Object.values(Locales).includes(router.query.locale[0] as Locales)
+                ? (router.query.locale[0] as Locales)
+                : Locales.en
+            : Locales.en
     return <RegistrationComponent locale={locale} />
 }
