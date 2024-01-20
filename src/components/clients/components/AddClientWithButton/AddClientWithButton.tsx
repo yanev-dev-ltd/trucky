1bff02b3561fbfce2d60773874c23ff8af8bb1a1
Dimitar Yanev <dimitar.m.yanev@gmail.com>
2024-01-20 17:34:19 +0200

    finished new select and orders

--- src/components/clients/components/AddClientWithButton/AddClientWithButton.tsx
+++ src/components/clients/components/AddClientWithButton/AddClientWithButton.tsx
@@ -31,7 +31,7 @@ const AddClientWithButton = () => {
             </Tooltip>
             <AddClient
                 open={open}
-                setOpen={setOpen}
+                setOpen={(isOpen) => setOpen(!!isOpen)}
                 onSave={() => setOpen(false)}
                 redirectToEdit
             />
